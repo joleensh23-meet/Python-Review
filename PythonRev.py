1a10392ec89be5a2509063f02a926727f21a44a0
joleensh23-meet <109904191+joleensh23-meet@users.noreply.github.com>
2022-07-25 18:37:34 +0300

    Update PythonRev.py

--- PythonRev.py
+++ PythonRev.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title,description):
-	new_video={"title":title , "description":description , "likes":0 , "dislikes":0 , "comments": {}}
+def create_youtube_video(title,description,list1):
+	new_video={"title":title , "description":description , "likes":0 , "dislikes":0 , "comments": {},"hashtag":list1}
 	return new_video
 	
 def likes(new_video):
@@ -19,7 +19,27 @@ def add_comment(youtubevideo,username,comment_text):
 	return youtubevideo
 
 newyoutubevideo = create_youtube_video("Eating video","Animals in the Rainforest")
-newyoutubevideo = likes(newyoutubevideo)
+for i in range(496):
+	newyoutubevideo = likes(newyoutubevideo)
 newyoutubevideo = dislikes(newyoutubevideo)
 newyoutubevideo = add_comment(newyoutubevideo , "username" , "comment")
 print(newyoutubevideo)
+
+def hashtag1():
+	list1=[]
+    i=0
+    a=0
+    print("Put . if you are done!")
+    while a!='.' and i<=5:
+    	a=input()
+    	list1.append(a)
+    	i+=1
+    if "." in list1:
+    	list1.remove(".")
+    return list1
+
+
+
+
+
+
